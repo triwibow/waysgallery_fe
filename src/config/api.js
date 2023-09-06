b881e44a281996eada8b9f83474af72bbe57e006
tri w <triw@oninyon.com>
2023-09-06 19:05:49 +0700

    -dd

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "https://waysgalleryapi-production.up.railway.app/api/v1"
+    baseURL: "https://waysgalleryapi-production.up.railway.app/api/v1",
+    headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': 'true',
+			'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+			'Access-Control-Allow-Headers': 'Set-Cookie'
+    },
 });
 
 const setAuthToken = (token) => {
